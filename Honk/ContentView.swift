568044648db14b5e26a4181cf4e5fc4b2ff7ff7b
Edward West <26151908+EdWest@users.noreply.github.com>
2022-05-07 21:19:55 -0700

    Initial Commit

---
+++ Honk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honk
+//
+//  Created by Ed West on 5/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Honk/HonkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HonkApp.swift
+//  Honk
+//
+//  Created by Ed West on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct HonkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
