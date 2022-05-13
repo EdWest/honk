069a45ffcdc76a880b92e71cefa9edc65073f555
Edward West <26151908+EdWest@users.noreply.github.com>
2022-05-12 22:47:48 -0700

    background

--- Honk/ContentView.swift
+++ Honk/ContentView.swift
@@ -15,27 +15,30 @@ struct ContentView: View {
             Button("Honk1234", action:soundplayer.playSound)
                 .frame(width: reader.size.width * 0.75)
                 .padding()
-                .background(Color.black)
-                .foregroundColor(Color.yellow) //Button text color is pea green now -- Ed 5/12/22
+                .background(Color.gray)
+                .foregroundColor(Color.black) //Button text color is pea green now -- Ed 5/12/22
                 .position(x: reader.size.width / 2, y: reader.size.height / 2)
 
             VStack(spacing:20) {
                 Text("This is Ed's Honk app")
                     .font(.largeTitle)
+                    .foregroundColor(Color.black)
                     Text("Press the button below to hear a Honk")
-                    .font(Font.custom("din1451alt", size: 18))
+                    .foregroundColor(Color.gray)
+                    //.font(Font.custom("din1451alt", size: 18))
                     Text("When pressed, a 12345 will sound")
-                    .font(Font.custom("din1451alt", size: 18))
+                    .foregroundColor(Color.gray)
+                    //.font(Font.custom("din1451alt", size: 18))
                     
             }
             .font(Font.custom("din1451alt", size: 18))
             .position(x: reader.size.width / 2, y: reader.size.height / 3)
         }
-        .background(Color.green) //background color is pea green now -- Ed 5/12/22
+        .background(Image("background").resizable())
         
-//       Image("mojave day") // This was supposed to be a background image 'mojave day'  -- Ed 5/12/22
-//        .resizable()
-//        .scaledToFill()
+
+        
+
 //
 //
 //        @StateObject var soundplayer:Soundplayer = Soundplayer2()  // This was supposed to be a second button  -- Ed 5/12/22
