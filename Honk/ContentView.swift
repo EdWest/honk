739c92be7a8b7567731f61e8ede34b0cc42c2f26
Edward West <26151908+EdWest@users.noreply.github.com>
2022-05-12 21:56:31 -0700

    Fifth commit

--- Honk/ContentView.swift
+++ Honk/ContentView.swift
@@ -12,29 +12,33 @@ struct ContentView: View {
     @StateObject var soundplayer:Soundplayer = Soundplayer()
     var body: some View {
         GeometryReader{reader in
-            Button("Honk", action:soundplayer.playSound)
+            Button("Honk1234", action:soundplayer.playSound)
                 .frame(width: reader.size.width * 0.75)
                 .padding()
                 .background(Color.black)
-                .foregroundColor(Color.white)
+                .foregroundColor(Color.yellow) //Button text color is pea green now -- Ed 5/12/22
                 .position(x: reader.size.width / 2, y: reader.size.height / 2)
 
-            VStack {
+            VStack(spacing:20) {
                 Text("This is Ed's Honk app")
                     .font(.largeTitle)
-
-                VStack {
-                    Text("     ")
                     Text("Press the button below to hear a Honk")
-                    Text("     ")
-                    Text("When pressed, a Honk will sound")
-                }
+                    .font(Font.custom("din1451alt", size: 18))
+                    Text("When pressed, a 12345 will sound")
+                    .font(Font.custom("din1451alt", size: 18))
+                    
             }
-            .font(.system(size: 16, weight: .light, design: .default))
+            .font(Font.custom("din1451alt", size: 18))
             .position(x: reader.size.width / 2, y: reader.size.height / 3)
         }
+        .background(Color.green) //background color is pea green now -- Ed 5/12/22
         
-//            @StateObject var soundplayer:Soundplayer = Soundplayer2()
+//       Image("mojave day") // This was supposed to be a background image 'mojave day'  -- Ed 5/12/22
+//        .resizable()
+//        .scaledToFill()
+//
+//
+//        @StateObject var soundplayer:Soundplayer = Soundplayer2()  // This was supposed to be a second button  -- Ed 5/12/22
 //            var body: some View {
 //                 GeometryReader{reader in
 //                  Button("door", action:soundplayer2.playSound)
@@ -46,9 +50,9 @@ struct ContentView: View {
  
         }
 
-    }
     
 
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
