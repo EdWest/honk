ff06d724de7a4ea421291194c1955f9dca24bcce
Edward West <26151908+EdWest@users.noreply.github.com>
2022-05-22 16:49:00 -0700

    HooliChat integration
    HooliChat

---
+++ node_modules/12/1.js
@@ -0,0 +1,15 @@
+exports.sum=function(){
+    var result = 0;
+    for(var i=0;i<arguments.length;i++){
+      result+=arguments[i];
+    }
+    return result;
+}
+
+exports.div=function(){
+    var result = 0;
+    for(var i=0;i<arguments.length;i++){
+        result*=arguments[i];
+    }
+    return result;
+}
---
+++ node_modules/12/index.js
@@ -0,0 +1,2 @@
+var mod  =require( "1" );
+console.log( mod.sum( 1,2,3,4 ) )
